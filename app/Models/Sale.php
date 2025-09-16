6abfa09f0d813e2254c88ae68d6e3bb9d382e511
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-16 16:28:39 -0600

    Se añadió la propiedad $fillable y $casts en el modelo Sale para mejorar la gestión de datos. Se implementaron relaciones con los modelos Salesdetail, Client, Company, User y Typedocument, así como una relación uno a uno con Dte, optimizando la estructura del modelo.

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,66 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nu_unico',
+        'nu_doc',
+        'acuenta',
+        'state',
+        'state_credit',
+        'totalamount',
+        'waytopay',
+        'typesale',
+        'date',
+        'user_id',
+        'typedocument_id',
+        'client_id',
+        'company_id',
+        'json',
+        'doc_related',
+        'id_contingencia',
+        'codigoGeneracion'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'state' => 'boolean',
+        'state_credit' => 'boolean',
+        'totalamount' => 'decimal:2'
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(Salesdetail::class, 'sale_id');
+    }
+
+    public function salesdetails()
+    {
+        return $this->hasMany(Salesdetail::class, 'sale_id');
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function typedocument()
+    {
+        return $this->belongsTo(Typedocument::class);
+    }
+
+    public function dte()
+    {
+        return $this->hasOne(Dte::class, 'sale_id');
+    }
 }
