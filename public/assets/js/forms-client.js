ff11e45a9279b85430e51bfddde95e828179c51b
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-26 15:29:57 -0600

    Se actualizó la máscara de entrada del campo 'pasaporteedit' para aceptar un formato alfanumérico de 10 caracteres, mejorando la validación de datos en el formulario de edición de clientes.

--- public/assets/js/forms-client.js
+++ public/assets/js/forms-client.js
@@ -178,7 +178,7 @@ $(document).ready(function () {
 
     $("#ncredit").inputmask("999999-9");
     $("#nitedit").inputmask("99999999-9");
-    $("#pasaporteedit").inputmask("99999999-9");
+    $("#pasaporteedit").inputmask("AAAAAAAAAA");
     $("#tel1edit").inputmask("9999-9999");
     $("#tel2edit").inputmask("9999-9999");
 
