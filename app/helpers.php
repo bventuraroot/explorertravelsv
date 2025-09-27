70e44b5e44c9b3aa5ca8100f5a64a98b256a78a0
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-27 14:34:35 -0600

    aa

--- app/helpers.php
+++ app/helpers.php
@@ -651,7 +651,7 @@ if (!function_exists('fac')) {
             "descActividad"         => ($cliente[0]->codActividad == '0' or is_null($cliente[0]->codActividad) or $cliente[0]->codActividad == 'N/A') ? null : $cliente[0]->descActividad,
 
         ];
-        //dd($receptor);
+        dd($receptor);
 
         if ($cliente[0]->codPais == '9300') {
 
