bcd3b3715aa4580d46c730e05060cd84c4475f85
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-29 21:17:23 -0600

    aa

--- app/helpers.php
+++ app/helpers.php
@@ -882,7 +882,7 @@ if (!function_exists('fac')) {
             "numPagoElectronico"    => ""
         ];
         $es_mayor = ($totales["totalPagar"] >= 200);
-        dd($cliente[0]->nit);
+        dd($cliente[0]->extranjero);
         $extension = [
             "nombEntrega"   => ($es_mayor) ? $encabezado->NombreUsuario : null,
             "docuEntrega"   => ($es_mayor) ? str_replace("-", "", $encabezado->docUser) : null,
