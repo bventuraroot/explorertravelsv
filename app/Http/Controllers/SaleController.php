5d595197ba4a94b93486dd20e88e2437e4d5c0bc
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-30 09:48:08 -0600

    Se agregó el campo 'extranjero' en la consulta de la función 'destroy' en 'SaleController.php' para incluir información adicional sobre los clientes.a

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -1729,7 +1729,8 @@ class SaleController extends Controller
         36 tipoDocumentoCliente,
         d.code codPais,
         d.name nombrePais,
-        0 siempre_retiene_renta
+        0 siempre_retiene_renta,
+        a.extranjero
     FROM clients a
     INNER JOIN economicactivities b ON a.economicactivity_id=b.id
     INNER JOIN addresses c ON a.address_id=c.id
