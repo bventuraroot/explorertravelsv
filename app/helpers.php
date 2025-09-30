8de52ef9db65ea185e083e6eb940279e45be35cd
Brian Ventura <briandagoberto20@hotmail.com>
2025-09-29 21:27:23 -0600

    Se comentó la línea de depuración 'dd($comprobante)' en la función 'fac' en 'helpers.php' para evitar la interrupción del flujo de ejecución, manteniendo el código limpio y funcional.

--- app/helpers.php
+++ app/helpers.php
@@ -923,7 +923,7 @@ if (!function_exists('fac')) {
         $comprobante["extension"]                = $extension;
         $comprobante["apendice"]                 = null;
         //$comprobante2 = [];
-        dd($comprobante);
+        //dd($comprobante);
         return ($comprobante);
     }
 }
