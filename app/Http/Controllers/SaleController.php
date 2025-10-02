10e7e6f9b773f9391504295d47fd693e15f2cae9
Brian Ventura <briandagoberto20@hotmail.com>
2025-10-02 13:18:00 -0600

    aa

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2282,7 +2282,7 @@ class SaleController extends Controller
         $comprobante = json_decode($factura, true);
         //dd(json_decode($comprobante[0]["json"]));
         //dd(json_decode($comprobante[0]["json"]));
-        $data = json_decode($comprobante[0]["json"]['json']["json_enviado"], true);
+        $data = json_decode($comprobante[0]["json"]["json"]["json_enviado"], true);
         //print_r($data);
         //dd($data);
         //$tipo_comprobante = $data["documento"][0]["tipodocumento"];
